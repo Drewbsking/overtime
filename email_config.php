333ef5beb0b5a06a08dd98a71c02d61d506bcf4a
Drewbsking <157226040+Drewbsking@users.noreply.github.com>
2025-11-25 13:34:35 -0500

    refactor: Remove deprecated files and clean up the repository

--- email_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// SMTP configuration
-define('SMTP_HOST', 'overtime.rcocwiki.org');
-define('SMTP_PORT', 587);
-define('SMTP_USER', 'admin@overtime.rcocwiki.org');
-define('SMTP_PASS', 'Facts!Food');
-define('FROM_EMAIL', 'admin@overtime.rcocwiki.org');
-define('FROM_NAME', 'Admin');
-?>
